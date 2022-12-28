61ea3b041a70cd3679f1bafa303609955f303f92
Shubham Solanki <115726819+Solankishhubham@users.noreply.github.com>
2022-12-28 14:46:38 +0530

    Create promise1.js

---
+++ promise1.js
@@ -0,0 +1,14 @@
+new Promise((resolve,reject)=>{
+    new setTimeOut(() => {
+        salary credited=true
+        if(salarycredited===true && salary>costofps5){
+            resolve('buy him a ps3')
+        }
+        else if(salarycredited===true && salary>costofps5){
+            reject('buy him a ps4')
+        }
+        else{
+            reject('sorry son,i will try nwxt month')
+        }
+    },10*24*60)
+})
